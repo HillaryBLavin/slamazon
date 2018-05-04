81d5f897170d45d653efc6e778c55edcfc43b5c0
Hillary Lavin <hilavin@gmail.com>
2018-05-04 15:15:00 -0400

    Create and call customerPrompt fx, beautify file

--- slamazonCustomer.js
+++ slamazonCustomer.js
@@ -2,29 +2,47 @@ var mysql = require("mysql"),
     inquire = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "backend1279!",
-  database: "slamazonDB"
+    // Your password
+    password: "backend1279!",
+    database: "slamazonDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  makeTable();
-  connection.end();
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    makeTable();
+    connection.end();
 });
 
 var makeTable = function () {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         console.log("\nWelcome to Slamazon!\n");
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + "$" + res[i].price + " || " + res[i].stock_quantity + "\n");
         }
+        customerPrompt(res);
     })
-  }
+}
+
+var customerPrompt = function (res) {
+    inquire.prompt([{
+        type: 'input',
+        name: 'choice',
+        message: 'Please select an item for purchase [Quit with Q]'
+    }]).then(function (ans) {
+        var correct = false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name == ans.choice) {
+                correct = true;
+                var product = ans.choice;
+                var id = i;
+            }
+        }
+    })
+}
