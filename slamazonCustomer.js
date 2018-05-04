0516062068105645672e80b8316cfdc2a87bad28
Hillary Lavin <hilavin@gmail.com>
2018-05-04 15:02:19 -0400

    Create and call makeTable fx

--- slamazonCustomer.js
+++ slamazonCustomer.js
@@ -16,5 +16,15 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  makeTable();
   connection.end();
 });
+
+var makeTable = function () {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log("\nWelcome to Slamazon!\n");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + "$" + res[i].price + " || " + res[i].stock_quantity + "\n");
+        }
+    })
+  }
