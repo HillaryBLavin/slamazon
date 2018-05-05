67113bbca59753ba5ba07e84deb47051b446717d
Hillary Lavin <hilavin@gmail.com>
2018-05-04 22:22:24 -0400

    Update customerPrompt fx (special thanks to @mcrigler)

--- slamazonCustomer.js
+++ slamazonCustomer.js
@@ -17,12 +17,11 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    console.log("\n************************** Welcome to Slamazon! **************************\n");
     productTable();
-    connection.end();
 });
 
 var productTable = function() {
-    console.log("\n************************** Welcome to Slamazon! **************************\n");
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
 
@@ -40,3 +39,56 @@ var productTable = function() {
     });
 };
 
+var customerPrompt = function() {
+    inquire.prompt([
+        {
+            type: 'input',
+            message: 'Please enter the Item # of the product you wish to purchase.',
+            name: 'choice'
+        },
+        {
+            type: 'input',
+            message: "How many would you like to purchase?",
+            name: 'qty'
+        }
+    ]).then(function(order) {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            var i = order.choice - 1; 
+            if(res[i].stock_quantity >= order.qty) {
+                var updateQty = parseInt(res[i].stock_quantity) - parseInt(order.qty);
+                var total = parseFloat(res[i].price) * parseFloat(order.qty);
+                    total = total.toFixed(2);
+                connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity: updateQty}, {item_id: order.choice}], function(error, results) {
+                    if (error) throw error; 
+                    console.log("Congratulations on your purchase! Your total comes to $ " + total + "\n");
+                    continueShopping();
+                });
+            } else {
+                console.log("We apologize for the inconvenience, but we do not have enough stock to fulfill your order. Please make another selection.");
+                productTable();
+            }
+        });
+    });
+}
+
+function continueShopping() {
+    inquire.prompt([
+        {
+            type: 'confirm',
+            message: 'Would you like to continue shopping?',
+            name: 'continue'
+        }
+    ]).then(function (shopping) {
+        if (shopping.continue) {
+            productTable();
+        } else {
+            exitSlamazon();
+        }
+    });
+}
+
+function exitSlamazon() {
+    connection.end();
+    console.log("\n**************************Thanks for shopping! See you soon! **************************\n");
+}
